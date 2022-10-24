8ec8a29245fdd4042a422cada685852e2f596c48
AngelinaRepina <104089541+AngelinaRepina@users.noreply.github.com>
2022-10-24 17:51:12 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,40 +1,77 @@
-move = 3
-message = str(input("Enter your message to encrypt (in lowercase): "))
-result = ''
-alphabets_in_lowercase=[]
-for i in range(97,123):
-    alphabets_in_lowercase.append(chr(i))
-alphabet = (alphabets_in_lowercase)
-alph = ''.join(alphabet)
-t = len(alph)
-print (alph)
-def encrypt_caesar (plaintext = message, shift = move):
-    ciphertext = ' '
-    for i in plaintext: 
-        place = alph.find(i)
-        new_place = place + shift
-        if new_place >= t:
-            new_place = new_place - t    
-        if i in alph:
-            ciphertext += alph[new_place]
-        else:
-            ciphertext += i
+ def encrypt_caesar (plaintext: str, shift: int = 3) -> str:
+    ciphertext = " "
+    alphabets_in_lowercase = []
+    alphabets_in_uppercase = []
+    for i in range(65, 91):
+        alphabets_in_uppercase.append(chr(i))
+    alph_in_uppercase = (alphabets_in_uppercase)
+    alph_in_upper = ''.join(alph_in_uppercase)
+    t = len(alph_in_upper)
+    print (alph_in_upper)
+    for i in range(97, 123):
+        alphabets_in_lowercase.append(chr(i))
+    alph_in_lowercase = (alphabets_in_lowercase)
+    alph_in_lower = ''.join(alph_in_lowercase)
+    k = len(alph_in_lower)
+    print (alph_in_lower)
+    new_place: int = 0
+    place: int = 0
+    for i in plaintext:
+        place = (alph_in_upper.find(i))
+        if place in range(0, t):
+            new_place = place + shift
+            if new_place >= t:
+                new_place = new_place - t
+            if i in alph_in_upper:
+                ciphertext += alph_in_upper[new_place]
+    
+        else: 
+            place = (alph_in_lower.find(i))
+            if place in range(0, k):
+                new_place = place + shift
+                if new_place >= k:
+                    new_place = new_place - k
+                if i in alph_in_lower:
+                    ciphertext += alph_in_lower[new_place]
     print (ciphertext)
     return ciphertext
-print (encrypt_caesar())
-
-message_to_decrypt = str(input("Enter your message to decrypt (in lowercase):"))
-def decrypt_caesar (ciphertext = message_to_decrypt, shift = move):
-    plaintext = ''
+print (encrypt_caesar("Hello*"))
+                        
+def decrypt_caesar (ciphertext: str, shift: int = 3) -> str:
+    plaintext = " "
+    alphabets_in_lowercase = []
+    alphabets_in_uppercase = []
+    for i in range(65, 91):
+        alphabets_in_uppercase.append(chr(i))
+    alph_in_uppercase = (alphabets_in_uppercase)
+    alph_in_upper = ''.join(alph_in_uppercase)
+    t = len(alph_in_upper)
+    print (alph_in_upper)
+    for i in range (97, 123):
+        alphabets_in_lowercase.append(chr(i))
+    alph_in_lowercase = (alphabets_in_lowercase)
+    alph_in_lower = ''.join(alph_in_lowercase)
+    k = len(alph_in_lower)
+    print (alph_in_lower)
+    place: int = 0
+    new_place: int = 0
     for i in ciphertext:
-        place = alph.find(i)
-        new_place = place - shift
-        if new_place < 0:
-            new_place = new_place + t
-        if i in alph:
-            plaintext += alph[new_place]
+        place = (alph_in_upper.find(i))
+        if place in range(0, t):
+            new_place = place - shift
+            if new_place < 0:
+                new_place = new_place + t
+            if i in alph_in_upper:
+                plaintext += alph_in_upper[new_place]
+                
         else:
-            plaintext += i
+            place = (alph_in_lower.find(i))
+            if place in range(0, k):
+                new_place = place - shift
+                if new_place < 0:
+                    new_place = new_place + k
+                if i in alph_in_lower:
+                    plaintext += alph_in_lower[new_place]
     print (plaintext)
     return plaintext
-print (decrypt_caesar())
+print (decrypt_caesar("Khoor*"))
